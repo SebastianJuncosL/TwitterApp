d1706789fc555fb6e2c37d86dfb9104fe9476cf0
SebastianJuncosL <sebastianjuncos@gmail.com>
2021-06-29 18:59:07 -0500

    Moved the relativeTimeAgo function from TweetsAdapter to Tweet so the adapter doesn't work much

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,7 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Locale;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
 
@@ -71,46 +67,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             tvBody.setText(tweet.body);
             tvScreenName.setText(tweet.user.screenName);
             Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
-            tvRelativeTime.setText(getRelativeTimeAgo(tweet.createdAt));
+            tvRelativeTime.setText(tweet.timeAgo);
         }
-
-        private static final int SECOND_MILLIS = 1000;
-        private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
-        private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
-        private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
-
-        public String getRelativeTimeAgo(String rawJsonDate) {
-            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-            sf.setLenient(true);
-
-            try {
-                long time = sf.parse(rawJsonDate).getTime();
-                long now = System.currentTimeMillis();
-
-                final long diff = now - time;
-                if (diff < MINUTE_MILLIS) {
-                    return "just now";
-                } else if (diff < 2 * MINUTE_MILLIS) {
-                    return "a minute ago";
-                } else if (diff < 50 * MINUTE_MILLIS) {
-                    return diff / MINUTE_MILLIS + " m";
-                } else if (diff < 90 * MINUTE_MILLIS) {
-                    return "an hour ago";
-                } else if (diff < 24 * HOUR_MILLIS) {
-                    return diff / HOUR_MILLIS + " h";
-                } else if (diff < 48 * HOUR_MILLIS) {
-                    return "yesterday";
-                } else {
-                    return diff / DAY_MILLIS + " d";
-                }
-            } catch (ParseException e) {
-                // Log.i(TAG, "getRelativeTimeAgo failed");
-                e.printStackTrace();
-            }
-
-            return "";
-        }
-
     }
 }
